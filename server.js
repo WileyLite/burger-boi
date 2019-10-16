677dbedbb943b97fcc8b02a33030345a355027b1
Jalin Wiley <jmt.wiley@gmail.com>
2019-10-15 20:04:22 -0400

    Ran server with some difficulty oddly

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const expresshand = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
