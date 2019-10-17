fc95ea09b7a800af3610cc831d3ab67eaaf14cfd
Jalin Wiley <jmt.wiley@gmail.com>
2019-10-17 00:50:01 -0400

    Server errors but pushing anyway

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,26 @@
-var orm = require("../config/orm");
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.selectAll("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColsVals, condition, cb) {
+        orm.selectAll("burgers", objColsVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        orm.selectAll("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+module.exports = burger;
